9ab10af2af22deee2e6cd0945db8dfafdbca27ac
Joaquin <65967417+joaquingavilan@users.noreply.github.com>
2024-05-27 21:52:01 -0400

    Update settings.py

--- ProyectosAldIA/settings.py
+++ ProyectosAldIA/settings.py
@@ -140,8 +140,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'Aplicacion/static'),
 )
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
