aacbf3c218078aeb72726273c8c2ec07fb43b8d6
Joaquin <65967417+joaquingavilan@users.noreply.github.com>
2024-05-27 20:20:24 -0400

    Update settings.py

--- ProyectosAldIA/settings.py
+++ ProyectosAldIA/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'ProyectosAldIA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Aplicacion', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
